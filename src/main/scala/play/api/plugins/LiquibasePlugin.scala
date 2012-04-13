27c9578cbebbd64a01db55fdee8797bc47e4a009
Kanischev Albert <kanischev@gmail.com>
2012-04-13 10:58:36 +0400

    Fixed connection usage

--- src/main/scala/play/api/plugins/LiquibasePlugin.scala
+++ src/main/scala/play/api/plugins/LiquibasePlugin.scala
@@ -1,12 +1,12 @@
 package play.api.plugins
 
-import play.api.db.DBPlugin
 import liquibase.Liquibase
 import scala.collection.JavaConversions._
 import liquibase.changelog.ChangeSet
 import liquibase.database.jvm.JdbcConnection
 import liquibase.resource.FileSystemResourceAccessor
 import play.api._
+import db.{DB, DBPlugin}
 
 /**
  * @date: 03.04.12
@@ -33,19 +33,21 @@ class LiquibasePlugin(app: Application) extends Plugin {
     api.datasources.foreach {
       case (ds, dbName) => {
         val fileOpener = new FileSystemResourceAccessor(app.path.getAbsolutePath)
-        val liqui = new Liquibase("conf/liquibase/" + dbName + "/modules.xml", fileOpener, new JdbcConnection(ds.getConnection))
-        app.mode match {
-          case Mode.Test => liqui.update(TestContext)
-          case Mode.Dev if app.configuration.getBoolean("applyLiquibase." + dbName).filter(_ == true).isDefined => liqui.update(DeveloperContext)
-          case Mode.Prod if app.configuration.getBoolean("applyLiquibase." + dbName).filter(_ == true).isDefined => liqui.update(ProductionContext)
-          case Mode.Prod => {
-            Logger("play").warn("Your production database [" + dbName + "] needs Liquibase updates! \n\n" + getScriptDescriptions(liqui.listUnrunChangeSets(ProductionContext)))
-            Logger("play").warn("Run with -DapplyLiquibase." + dbName + "=true if you want to run them automatically (be careful)")
+        DB.withConnection(dbName)(connection => {
+          val liqui = new Liquibase("conf/liquibase/" + dbName + "/modules.xml", fileOpener, new JdbcConnection(connection))
+          app.mode match {
+            case Mode.Test => liqui.update(TestContext)
+            case Mode.Dev if app.configuration.getBoolean("applyLiquibase." + dbName).filter(_ == true).isDefined => liqui.update(DeveloperContext)
+            case Mode.Prod if app.configuration.getBoolean("applyLiquibase." + dbName).filter(_ == true).isDefined => liqui.update(ProductionContext)
+            case Mode.Prod => {
+              Logger("play").warn("Your production database [" + dbName + "] needs Liquibase updates! \n\n" + getScriptDescriptions(liqui.listUnrunChangeSets(ProductionContext)))
+              Logger("play").warn("Run with -DapplyLiquibase." + dbName + "=true if you want to run them automatically (be careful)")
 
-            throw PlayException("Liquibase script should be applyed, set applyLiquibase."+dbName+"=true in application.conf", getScriptDescriptions(liqui.listUnrunChangeSets(ProductionContext)))
+              throw PlayException("Liquibase script should be applyed, set applyLiquibase."+dbName+"=true in application.conf", getScriptDescriptions(liqui.listUnrunChangeSets(ProductionContext)))
+            }
+            case _ => PlayException("Liquibase script should be applyed, set applyLiquibase."+dbName+"=true in application.conf", getScriptDescriptions(liqui.listUnrunChangeSets(ProductionContext)))
           }
-          case _ => PlayException("Liquibase script should be applyed, set applyLiquibase."+dbName+"=true in application.conf", getScriptDescriptions(liqui.listUnrunChangeSets(ProductionContext)))
-        }
+        })(app)
       }
     }
   }
